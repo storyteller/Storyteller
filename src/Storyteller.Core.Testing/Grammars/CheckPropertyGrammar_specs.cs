e2c8998c99b099cf48a2eeb073a9d7c9a7016f2b
jeremydmiller <jeremydmiller@yahoo.com>
2015-01-23 12:41:33 -0700

    added CheckPropertyGrammar to Three

---
+++ src/Storyteller.Core.Testing/Grammars/CheckPropertyGrammar_specs.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Storyteller.Core.Grammars;
+
+namespace Storyteller.Core.Testing.Grammars
+{
+    [TestFixture]
+    public class CheckPropertyGrammar_specs : SpecRunningContext
+    {
+        [Test]
+        public void execute()
+        {
+            execute(@"
+=> CheckProperty
+* SetName: name=Jeremy
+* Check#1: Name=Jeremy
+* Check#2: Name=Max
+
+");
+
+            CountsShouldBe(1, 1, 0, 0);
+
+            Step("1").Cell("Name").Succeeded();
+            Step("2").Cell("Name").FailedWithActual("Jeremy");
+        }
+    }
+
+    public class CheckPropertyFixture : Fixture
+    {
+        public CheckPropertyFixture()
+        {
+            this["Check"] = CheckPropertyGrammar.For<CheckPropertyTarget>(x => x.Name);
+        }
+
+        public void SetName(string name)
+        {
+            Context.State.CurrentObject = new CheckPropertyTarget {Name = name};
+        }
+
+
+    }
+
+    public class CheckPropertyTarget
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Storyteller.Core/Grammars/CheckPropertyGrammar.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using FubuCore;
+using FubuCore.Reflection;
+using Storyteller.Core.Conversion;
+using Storyteller.Core.Grammars.Lines;
+using Storyteller.Core.Model;
+using Storyteller.Core.Results;
+
+namespace Storyteller.Core.Grammars
+{
+    // TODO -- add default values here
+    public class CheckPropertyGrammar : LineGrammar
+    {
+        private readonly Accessor _accessor;
+        private Cell _cell;
+
+        public CheckPropertyGrammar(Accessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public override IEnumerable<CellResult> Execute(StepValues values, ISpecContext context)
+        {
+            var actual = _accessor.GetValue(context.State.CurrentObject);
+            var expected = values.Get(_cell.Key);
+
+            if (expected.Equals(actual))
+            {
+                yield return CellResult.Success(_cell.Key);
+            }
+            else
+            {
+                yield return CellResult.Failure(_cell.Key, actual.ToString());
+            }
+        }
+
+        protected override string format()
+        {
+            return "{0} should be {{{0}}}".ToFormat(_accessor.Name);
+        }
+
+        protected override IEnumerable<Cell> buildCells(CellHandling cellHandling)
+        {
+            _cell = new Cell(cellHandling, _accessor.Name, _accessor.PropertyType);
+            return new[] {_cell};
+        }
+
+
+
+
+        public static CheckPropertyGrammar For<T>(Expression<Func<T, object>> expression)
+        {
+            Accessor accessor = ReflectionHelper.GetAccessor(expression);
+            return new CheckPropertyGrammar(accessor);
+        }
+
+
+    }
+}
