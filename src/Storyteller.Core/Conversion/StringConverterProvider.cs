41b1d028e058af578dd25cfdfc1ad284c8221fa6
Alistair Bush <alistair.bush@gmail.com>
2015-02-02 21:12:47 +1300

    Support the ctor(string) conversion pattern

---
+++ src/Storyteller.Core/Conversion/StringConverterProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Storyteller.Core.Conversion;
+using FubuCore;
+using System.Linq.Expressions;
+
+namespace Storyteller.Core
+{
+    public class StringConverterProvider : IConversionProvider
+    {
+        public Func<string, object> ConverterFor (Type type)
+        {
+            if (!type.IsConcrete()) {
+                return null;
+            }
+            var constructor = type.GetConstructor (new [] { typeof(string) });
+            if (constructor == null) {
+                return null;
+            }
+
+            var param = Expression.Parameter (typeof(string), "arg");
+            var body = Expression.New (constructor, param);
+
+            return Expression.Lambda<Func<string, object>> (body, param).Compile();
+        }
+    }
+}
+
