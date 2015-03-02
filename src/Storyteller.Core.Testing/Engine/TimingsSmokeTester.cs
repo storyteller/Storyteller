c0e0065f62def3eb77eb52ad162ba6be38a5f021
jeremydmiller <jeremydmiller@yahoo.com>
2015-03-02 06:57:43 -0600

    built the Timings class for perf watching

---
+++ src/Storyteller.Core.Testing/Engine/TimingsSmokeTester.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using FubuTestingSupport;
+using NUnit.Framework;
+using Storyteller.Core.Engine;
+using Storyteller.Core.Model;
+
+namespace Storyteller.Core.Testing.Engine
+{
+    [TestFixture]
+    public class TimingsSmokeTester
+    {
+        [Test]
+        public void record_data()
+        {
+            var spec = new Specification
+            {
+                Name = "Some Name"
+            };
+
+            var timings = new Timings(spec);
+            using (timings.Start("Fixture.Setup", "Math"))
+            {
+                using (timings.Start("Grammar", "Adding"))
+                {
+                    using (timings.Start("Fixture.Teardown", "Math"))
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+
+            var records = timings.Finish();
+
+            records.Select(x => x.Subject).ShouldHaveTheSameElementsAs("Some Name", "Math", "Adding", "Math");
+
+            records.Each(x => x.Duration.ShouldBeGreaterThan(0));
+        }
+    }
+}
---
+++ src/Storyteller.Core/Engine/Timings.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using FubuCore.Binding.InMemory;
+using Newtonsoft.Json;
+using Storyteller.Core.Model;
+
+namespace Storyteller.Core.Engine
+{
+    public class Timings
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        private readonly IList<PerfRecord> _records = new List<PerfRecord>();
+        private readonly PerfRecord _main;
+
+        public Timings(Specification spec)
+        {
+            _main = new PerfRecord("Specification", spec.Name, 0);
+            _records.Add(_main);
+            _stopwatch.Start();
+        }
+
+        public IDisposable Start(string type, string subject)
+        {
+            var record = new PerfRecord(type, subject, _stopwatch.ElapsedMilliseconds);
+            _records.Add(record);
+
+            return new Marker(record, _stopwatch);
+        }
+
+        public IEnumerable<PerfRecord> Finish()
+        {
+            _stopwatch.Stop();
+            _main.MarkEnd(_stopwatch.ElapsedMilliseconds);
+
+            return _records.OrderBy(x => x.Start).ToArray();
+        }
+
+        public class Marker : IDisposable
+        {
+            private readonly PerfRecord _record;
+            private readonly Stopwatch _stopwatch;
+
+            public Marker(PerfRecord record, Stopwatch stopwatch)
+            {
+                _record = record;
+                _stopwatch = stopwatch;
+            }
+
+            public void Dispose()
+            {
+                _record.MarkEnd(_stopwatch.ElapsedMilliseconds);
+            }
+        }
+    }
+
+    public class PerfRecord
+    {
+        public PerfRecord(string type, string subject, long start)
+        {
+            Type = type;
+            Subject = subject;
+            Start = start;
+        }
+
+        public void MarkEnd(long end)
+        {
+            End = end;
+        }
+
+        [JsonProperty("type")]
+        public string Type { get; private set; }
+
+        [JsonProperty("subject")]
+        public string Subject { get; private set; }
+
+        [JsonProperty("start")]
+        public long Start { get; private set; }
+
+        [JsonProperty("end")]
+        public long End { get; private set; }
+
+        [JsonProperty("duration")]
+        public long Duration
+        {
+            get { return End - Start; }
+        }
+
+    }
+}
