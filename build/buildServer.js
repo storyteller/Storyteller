5dd583a5de1efffbab585720125aefa093c7ea4c
jeremydmiller <jeremydmiller@yahoo.com>
2015-03-25 10:14:43 -0500

    turning on ripple nuget publishing for CI builds

--- build/buildServer.js
+++ build/buildServer.js
@@ -4,8 +4,16 @@ var buildVersion = '3.0.0.' + buildNumber;
 var revision = args.git || 'unknown';
 var config = args.config || 'Debug';
 
+var publish = args.publish || false;
+
 var assemblyInfo = require('./assemblyInfo');
 assemblyInfo(buildVersion, revision);
 
 require('./compile')(config);
 require('./fixie')(config);
+
+if (publish){
+	var exec = require('child_process').execSync;
+	var cmd = 'ripple publish ' + buildVersion + ' ' + args.apikey + ' -s source https://www.myget.org/F/fubumvc-edge';
+	var output = exec('ripple publish ')
+}
