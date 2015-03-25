912d1901f6a477f3d8bfc1ad6955a951e8525a80
jeremydmiller <jeremydmiller@yahoo.com>
2015-03-25 11:10:56 -0500

    switching to paket for creating nugets

--- src/StoryTeller/AssemblyInfo.cs
+++ src/StoryTeller/AssemblyInfo.cs
@@ -6,3 +6,5 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Storyteller")]
+[assembly: AssemblyDescription("Executable Specifications for .Net")]
+[assembly: AssemblyCompany("Jeremy D. Miller, Brandon Behrens, Andrew Kharlamov, et al")]
