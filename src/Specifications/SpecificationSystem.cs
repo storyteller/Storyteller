b3d8654371ac3a96bf556da57b4b5a1b42dda6c8
dpen2000 <davidpendray@gmail.com>
2015-04-02 08:46:20 +0100

    Fix breaking build

--- src/Specifications/SpecificationSystem.cs
+++ src/Specifications/SpecificationSystem.cs
@@ -4,6 +4,19 @@ namespace Specifications
 {
     public class SpecificationSystem : ISystem
     {
-         
+        public IExecutionContext CreateContext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public StoryTeller.CellHandling Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
