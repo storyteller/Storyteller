b7020ea255686b776d5561c82267d380c6b4f26a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-04-03 05:53:58 -0500

    Keyboard shortcut for saving a spec.
    Closes GH-194

--- client/lib/editor-presenter.js
+++ client/lib/editor-presenter.js
@@ -118,6 +118,7 @@ class EditorPresenter{
 		this.subscribe('go-next', () => this.moveNext());
 		this.subscribe('go-previous', () => this.movePrevious());
 		this.subscribe('run-spec', () => this.run());
+		this.subscribe('save-spec', () => this.save());
 
 		this.subscribe('spec-results-changed', function(data){
 			if (data.id == self.id){
