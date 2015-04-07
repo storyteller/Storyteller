baa946c843fc668146c47727c05af25d36a2425e
jeremydmiller <jeremydmiller@yahoo.com>
2015-04-07 15:18:03 -0500

    Fixed mark-as-acceptance topic to match the client

--- src/StoryTeller/Messages/MarkAsAccepted.cs
+++ src/StoryTeller/Messages/MarkAsAccepted.cs
@@ -2,7 +2,7 @@ namespace StoryTeller.Messages
 {
     public class MarkAsAccepted : ClientMessage
     {
-        public MarkAsAccepted() : base("mark-as-accepted")
+        public MarkAsAccepted() : base("mark-as-acceptance")
         {
         }
 
