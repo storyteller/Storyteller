ce5b3c8248fb71c76d76b23ea6a83e8f021a00f2
jeremydmiller <jeremydmiller@yahoo.com>
2015-04-22 14:16:35 -0500

    Changed the SnippetTag to match prism.js

--- src/ST/Docs/Samples/SnippetTag.cs
+++ src/ST/Docs/Samples/SnippetTag.cs
@@ -1,26 +1,13 @@
 using System;
-using FubuCore;
 using HtmlTags;
 
 namespace ST.Docs.Samples
 {
-    // TODO -- change this when we go to Prism.js
     public class SnippetTag : HtmlTag
     {
         public SnippetTag(Snippet snippet) : base("pre")
         {
-            Text(Environment.NewLine + snippet.Text);
-            AddClass("prettyprint");
-
-            if (snippet.Language.IsNotEmpty())
-            {
-                AddClass(snippet.Language);
-            }
-
-            if (snippet.Start > 0)
-            {
-                Data("linenums", snippet.Start);
-            }
+            Add("code").AddClass("language-" + snippet.Language).Text(Environment.NewLine + snippet.Text);
         }
     }
 }
