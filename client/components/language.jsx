910845f02444ee20ca2f33864d7f8189d17eb0ee
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-05-28 15:09:42 -0500

    Little bit of a new "Language Explorer"

--- client/components/language.jsx
+++ client/components/language.jsx
@@ -1,12 +1,66 @@
 /** @jsx React.DOM */
 
 var React = require("react");
+var Hierarchy = require('./../lib/specs/hierarchy');
+var Postal = require('postal');
 
+
+var FixtureTable = React.createClass({
+	render(){
+		var fixtures = _.sortBy(_.values(this.props.library.fixtures), x => x.title);
+
+		var rows = fixtures.map(x => {
+			return (
+				<tr>
+					<td>{x.title}</td>
+					<td>{x.implementation}</td>
+				</tr>
+			);
+		});
+
+		return (
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Fixture Title</th>
+						<th>Implementation</th>
+					</tr>
+				</thead>
+				<tbody>
+					{rows}
+				</tbody>
+			</table>
+		);
+	}
+});
 
 module.exports = React.createClass({
+	componentDidMount(){
+		this.subscription = Postal.subscribe({
+			channel: 'explorer',
+			topic: 'fixtures-loaded',
+			callback: data => {
+				this.setState({
+					x: 0
+				});
+			}
+		});
+	},
+
+	componentWillUnMount(){
+		this.subscription.unsubscribe();
+	},
+
 	render: function(){
+		var library = Hierarchy.fixtures();
+
 		return (
-				<div>A representation of the Fixtures and Grammars</div>
+			<div>
+				<h2>The Specification Language</h2>
+				<hr />
+				<FixtureTable library={library} />
+
+			</div>
 		);
 	}
 
