d2fc91337dd3c17ee2c9e394611fb1e4030e9eda
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-07-01 09:30:42 -0500

    Added a selectFirst() implementation to Sentence. Closes GH-415

---
+++ src/Samples/Fixtures/CrazyParagraphFixture.cs
@@ -0,0 +1,30 @@
+﻿using StoryTeller;
+using StoryTeller.Grammars.Tables;
+
+namespace Samples.Fixtures
+{
+    public class CrazyParagraphFixture : Fixture
+    {
+        [ExposeAsTable("Greetings!")]
+        public void SaySomething(string Greeting, string To)
+        {
+            
+        }
+
+        public void Start(string when)
+        {
+            
+        }
+
+        public IGrammar Everything()
+        {
+            return Paragraph("Everything", _ =>
+            {
+                _ += this["Start"];
+                _ += this["SaySomething"];
+                
+
+            });
+        }
+    }
+}
