107fe0d7ffc0d51962a2bc8a8ba308904d07be1e
Ron Lloyd <Ronnie.Lloyd@extendhealth.com>
2015-08-06 10:29:57 -0600

    Step list floats and stays visible when scrolling down

--- client/components/editing/adders/step-adder.jsx
+++ client/components/editing/adders/step-adder.jsx
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 var React = require("react");
 var GrammarLookup = require('./../../../lib/presentation/grammar-adder-lookup');
+var $ = require('jquery');
 
 var AddStepItem = React.createClass({
 	render: function(){
@@ -22,16 +23,58 @@ var AddStepItem = React.createClass({
 });
 
 module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			containerStyle: {}
+		};
+	},
+
+	componentDidMount: function() {
+		var element = $(this.getDOMNode());
+
+		window.addEventListener('scroll', this.handleScroll);
+
+		this.setState({
+			offsetTop: element.offset().top,
+			width: element.width()
+		});
+	},
+
+	componentWillUnmount: function() {
+		window.removeEventListener('scroll', this.handleScroll);
+	},
+
+	handleScroll: function(event) {
+		var offsetTop = this.state.offsetTop;
+		var scrollOffset = $(window).scrollTop();
+
+		if (offsetTop < scrollOffset) {
+			this.setState({
+				containerStyle: {
+					position: 'fixed',
+					top: 0,
+					width: this.state.width
+				}
+			});
+		}
+		else {
+			this.setState({
+				containerStyle: {}
+			});
+		}
+	},
+
 	render: function(){
 		var holder = this.props.holder;
 		var lookup = new GrammarLookup(this.props.holder);
+		var containerStyle = this.state.containerStyle;
 
 		var components = lookup.options.map(x => {
 			return AddStepItem({option: x});
 		});
 
 		return (
-			<div>
+			<div style={containerStyle}>
 				<h5>Add items to {this.props.holder.title}</h5>
 				<div className="list-group add-steps-container">
 					{components}
