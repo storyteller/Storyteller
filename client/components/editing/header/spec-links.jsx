ae981d1547f0dbd7beb9d5f8412747416dc1f57f
Craig Jennings <craig.jennings11@gmail.com>
2015-08-26 10:25:19 -0500

    Conform file to .editorconfig settings

--- client/components/editing/header/spec-links.jsx
+++ client/components/editing/header/spec-links.jsx
@@ -4,46 +4,46 @@ var React = require("react");
 var {Button, ButtonGroup} = require('react-bootstrap');
 
 var LinkButton = React.createClass({
-	render: function(){
-		var onclick = e => {
-			window.location = this.props.href;
-			e.stopPropagation();
-		}
+  render: function(){
+    var onclick = e => {
+      window.location = this.props.href;
+      e.stopPropagation();
+    }
 
-		return (<Button onClick={onclick}>{this.props.text}</Button>);
-	}
+    return (<Button onClick={onclick}>{this.props.text}</Button>);
+  }
 });
 
 var SpecLinks = React.createClass({
-	buildLinks(){
-		var links = [];
-		if (this.props.mode != 'editing'){
-			var elem = (<LinkButton href={'#/spec/editing/' + this.props.id} text="Editor"/>);
-			links.push(elem);
-		}
+  buildLinks(){
+    var links = [];
+    if (this.props.mode != 'editing'){
+      var elem = (<LinkButton href={'#/spec/editing/' + this.props.id} text="Editor"/>);
+      links.push(elem);
+    }
 
-		if (this.props.mode != 'preview'){
-			var elem = (<LinkButton href={'#/spec/preview/' + this.props.id} text="Preview"/>);
-			links.push(elem);
-		}
+    if (this.props.mode != 'preview'){
+      var elem = (<LinkButton href={'#/spec/preview/' + this.props.id} text="Preview"/>);
+      links.push(elem);
+    }
 
-		if (this.props.mode != 'results'){
-			var elem = (<LinkButton href={'#/spec/results/' + this.props.id} text="Results" />);
-			links.push(elem);
-		}
+    if (this.props.mode != 'results'){
+      var elem = (<LinkButton href={'#/spec/results/' + this.props.id} text="Results" />);
+      links.push(elem);
+    }
 
-		return links;
-	},
+    return links;
+  },
 
-	render(){
-		var links = this.buildLinks();
+  render(){
+    var links = this.buildLinks();
 
-		return (
-			<ButtonGroup>
-				{links}
-			</ButtonGroup>
-		);
-	}
+    return (
+      <ButtonGroup>
+        {links}
+      </ButtonGroup>
+    );
+  }
 });
 
 module.exports = SpecLinks;
