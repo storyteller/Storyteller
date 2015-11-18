5d9e521f3cf1fbdfc3ba4d0c3dae23aed85bbe3b
jeremydmiller <jeremydmiller@yahoo.com>
2015-11-18 07:28:18 -0600

    added DataColumnMatch for usage later

---
+++ src/StoryTeller.Testing/Grammars/Sets/DataColumnMatchTester.cs
@@ -0,0 +1,66 @@
+﻿using System.Data;
+using FubuCore;
+using NUnit.Framework;
+using Shouldly;
+using StoryTeller.Grammars.Sets;
+using StoryTeller.Model;
+
+namespace StoryTeller.Testing.Grammars.Sets
+{
+    [TestFixture]
+    public class DataColumnMatchTester
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            table = new DataTable();
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("Distance", typeof(int));
+            table.Columns.Add("Zip", typeof(string));
+
+            table.Rows.Add("Austin", 16, "78704");
+            table.Rows.Add("London", 8192, "");
+            table.Rows.Add("Paris", 4096, null);
+
+            _matchCity = new DataColumnMatch("City", typeof(string));
+            _matchDistance = new DataColumnMatch("Distance", typeof(int));
+            _matchZip = new DataColumnMatch("Zip", typeof(string));
+        }
+
+        #endregion
+
+        private DataTable table;
+        private DataColumnMatch _matchCity;
+        private DataColumnMatch _matchDistance;
+        private DataColumnMatch _matchZip;
+
+
+        [Test]
+        public void get_cell()
+        {
+            Cell cell = _matchCity.As<IColumnMatch>().BuildCell(CellHandling.Basic(), new Fixture());
+            cell.Key.ShouldBe("City");
+            cell.Type.ShouldBe(typeof(string));
+        }
+
+        [Test]
+        public void build_cell_with_modifications()
+        {
+            _matchCity.CellModifications.Header("The City");
+            Cell cell = _matchCity.As<IColumnMatch>().BuildCell(CellHandling.Basic(), new Fixture());
+            cell.Key.ShouldBe("City");
+            cell.header.ShouldBe("The City");
+            cell.Type.ShouldBe(typeof(string));
+        }
+
+
+        [Test]
+        public void read_the_actual_values()
+        {
+            var row = table.Rows[2];
+            _matchCity.GetValue(row).ShouldBe("Paris");
+        }
+    }
+}
---
+++ src/StoryTeller/Grammars/Sets/DataColumnMatch.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Data;
+using FubuCore;
+using StoryTeller.Model;
+
+namespace StoryTeller.Grammars.Sets
+{
+    public class DataColumnMatch : IColumnMatch
+    {
+        private readonly Type _columnType;
+
+        public DataColumnMatch(string columnName, Type columnType)
+        {
+            Name = columnName;
+            _columnType = columnType;
+
+            CellModifications = new CellModifications();
+        }
+
+        public string Name { get; private set; }
+        public object GetValue(object actual)
+        {
+            var dataRow = actual.As<DataRow>();
+            assertColumnExists(dataRow);
+
+            return dataRow[Name] == DBNull.Value ? null : dataRow[Name];
+        }
+
+        public Cell BuildCell(CellHandling handling, Fixture fixture)
+        {
+            var cell = new Cell(handling, Name, _columnType);
+            CellModifications.Apply(cell);
+
+            return cell;
+        }
+
+        public CellModifications CellModifications { get; private set; }
+
+
+
+        private void assertColumnExists(DataRow row)
+        {
+            if (!row.Table.Columns.Contains(Name))
+            {
+                string message =
+                    "Requested column '{0}' does not exist in this table\nThe available columns are:\n".ToFormat(
+                        Name);
+                foreach (DataColumn column in row.Table.Columns)
+                {
+                    message += column.ColumnName + ", ";
+                }
+
+                StoryTellerAssert.Fail(message);
+            }
+        }
+    }
+}
