13220b0101e5fbfcb32897c3c75a09cd6ae8a4be
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-01-08 13:22:36 -0600

    Got the contextual pane displaying again on the editor page

--- client/components/editing/spec-editor.jsx
+++ client/components/editing/spec-editor.jsx
@@ -7,6 +7,10 @@ var EditorLoading = require('./alerts/editor-loading');
 var SpecHeader = require('./header/spec-header');
 var SpecResultHeader = require('./header/spec-result-header');
 var loader = require('./component-loader').editing;
+var RetryCount = require('./header/retry-count');
+var ExpirationPeriod = require('./header/expiration-period');
+var SpecOutline = require('./spec-outline');
+
 
 // TODO -- centralize this one!
 function getSpec(state, ownProps){
@@ -26,28 +30,42 @@ function getSpec(state, ownProps){
         });
     }
     
-    return {spec: spec, loading: loading};
+    // updatingDate: WHAT --> disables the ExpirationPeriod while it's being updated
+    
+    return {
+        spec: spec, 
+        loading: loading, 
+        activeContainer: spec.spec.activeContainer,
+        updatingDate: false
+    };
 }
 
 function addDispatch(dispatch){
     return {dispatch: dispatch};
 }
 
+function ContextualPane(props){
+    var contextualControl = null;
+    if (props.activeContainer){
+        contextualControl = props.activeContainer.contextualControl(loader);
+    }
+    
+    return (
+        <Col key="left" xs={4} md={4}>
+            <RetryCount count={props.spec.spec['max-retries']}/>
+            <ExpirationPeriod spec={props.spec.spec} disabled={props.updatingDate} />
+            <h4>Outline</h4>
+            <SpecOutline outline={props.outline} />
+            <br />
+            <br />
+            {contextualControl}
+        </Col>
+    );
+}
+
 function SpecEditor(props){
-    // TODO -- get the activeContainer?
-    // TODO -- get the outline?
-    // TODO -- get the updatingDate?
 
-/*
-                <ContextualPane
-                    spec={props.spec}
-                    loader={loader}
-                    activeContainer={this.state.activeContainer}
-        outline={this.state.outline}
-        updatingDate={this.state.updatingDate}
-        />
-*/
-
+    // TODO -- get the updatingDate? Think that would be something we do w/ redux now
     
     var components = props.spec.spec.editors(loader);
     
@@ -59,7 +77,13 @@ function SpecEditor(props){
             <SpecHeader spec={props.spec} mode="editing" />
             <Row>
                 
-
+				<ContextualPane
+                    spec={props.spec}
+                    loader={loader}
+                    activeContainer={props.activeContainer}
+                    outline={props.spec.spec.outline()}
+                    updatingDate={props.updatingDate}
+                />
                 
                 <Col key="right" xs={8} md={8}>
                     <SpecResultHeader spec={props.spec} />
