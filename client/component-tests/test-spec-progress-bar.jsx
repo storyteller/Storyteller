72e89fe7e2696c9470acd6b82a5f72c521075d29
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-01-16 14:01:20 -0600

    Brought back karma specs on ProgressBar

--- client/component-tests/test-spec-progress-bar.jsx
+++ client/component-tests/test-spec-progress-bar.jsx
@@ -1,71 +1,57 @@
 var React = require('react');
 var expect = require('chai').expect;
-var Postal = require('postal');
-var $ = require('jquery');
-var Hierarchy = require('./../lib/stores/hierarchy');
-
+var ComponentHarness = require('./component-harness');
 var SpecProgressBar = require('./../components/header/spec-progress-bar');
+var $ = require('jquery');
 
-var data = require('./../lib-tests/hierarchy-data');
-
-function publishEngineMessage(topic, data){
-	Postal.publish({
-		channel: 'engine',
-		topic: topic,
-		data: data
-	});
-}
-
-function hierarchyIsPublishedFromEngine(){
-	publishEngineMessage('hierarchy-loaded', {hierarchy: data});
-}
 
 describe('The SpecProgressBar', function(){
-	var component;
+	var component, harness;
 
 	beforeEach(function(){
-		Postal.reset();
-		Hierarchy.reset();
-
-		hierarchyIsPublishedFromEngine();
-
-		component = TestUtils.renderIntoDocument(
-		  <SpecProgressBar />
-		);
+		harness = new ComponentHarness();
+		component = (<SpecProgressBar />);
+        
+        harness.render(component);
 	});
 
 	it('shows nothing in the initial state', function(){
-		expect(component.getDOMNode()).to.be.null;
+        var element = harness.element();
+		expect(element.tagName).to.equal('SPAN');
+        expect(element.innerHTML).to.equal('');
 	});
 
 	it('shows the progress bar with no results with the initial steps but no counts', function(){
-		publishEngineMessage('spec-progress', {
+        harness.store.dispatch({
+            type: 'spec-progress',
 			id: 'embeds',
 			step: 3,
 			total: 10,
 			counts: {rights: 0, wrongs: 0, errors: 0, invalids: 0}
 		});
 
-		var progressBar = component.getDOMNode().children[1];
+		var progressBar = harness.element().children[1];
+
 		var span = $('span', progressBar).get(0);
 
 		expect(progressBar.tagName).to.equal('DIV');
-		expect(progressBar.className).to.equal('progress');
-		expect(progressBar.firstChild.className).to.equal('progress-bar');
+		expect($(progressBar.firstChild).attr('role')).to.equal('progressbar');
 
 		expect(span.getAttribute('min')).to.equal('0');
 		expect(span.getAttribute('max')).to.equal('10');
+        expect(progressBar.firstChild.className).to.equal('progress-bar progress-bar-info');
 	});
 
 	it('shows as succeeding if non-zero success counts with no wrongs or errors', function(){
-		publishEngineMessage('spec-progress', {
+        harness.store.dispatch({
+            type: 'spec-progress',
 			id: 'embeds',
 			step: 3,
 			total: 10,
 			counts: {rights: 3, wrongs: 0, errors: 0, invalids: 0}
 		});
 
-		var progressBar = component.getDOMNode().children[1];
+		var progressBar = harness.element().children[1];
 
 		expect(progressBar.tagName).to.equal('DIV');
 		expect(progressBar.firstChild.className).to.equal('progress-bar progress-bar-success');
@@ -73,14 +59,15 @@ describe('The SpecProgressBar', function(){
 	});
 
 	it('shows as failing if non-zero failures or errors', function(){
-		publishEngineMessage('spec-progress', {
+        harness.store.dispatch({
+            type: 'spec-progress',
 			id: 'embeds',
 			step: 3,
 			total: 10,
 			counts: {rights: 3, wrongs: 2, errors: 0, invalids: 0}
 		});
 
-		var progressBar = component.getDOMNode().children[1];
+		var progressBar = harness.element().children[1];
 
 		expect(progressBar.tagName).to.equal('DIV');
 		expect(progressBar.firstChild.className).to.equal('progress-bar progress-bar-danger');
@@ -88,7 +75,8 @@ describe('The SpecProgressBar', function(){
 	});
 
 	it('hides the progress bar after spec completion', function(){
-		publishEngineMessage('spec-progress', {
+        harness.store.dispatch({
+            type: 'spec-progress',
 			id: 'embeds',
 			step: 3,
 			total: 10,
@@ -98,8 +86,18 @@ describe('The SpecProgressBar', function(){
 		var results = {
 			counts: {rights: 0, wrongs: 0, errors: 1, invalids: 0}
 		}
-		expect(component.getDOMNode()).to.not.be.null;
-		publishEngineMessage('queue-state', {running: null, queued: []});
-		expect(component.getDOMNode()).to.be.null;
+
+		expect(harness.element().firstChild).to.not.be.null;
+
+        harness.store.dispatch({
+            type: 'queue-state',
+            running: null, 
+            queued: []
+        });
+        
+        var element = harness.element();
+		expect(element.tagName).to.equal('SPAN');
+        expect(element.innerHTML).to.equal('');
 	});
+    
 });
