07b32346676ba486746a620ccc753fb89d6f1123
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-01-20 22:24:19 -0600

    Highlighting the active sentence

--- client/components/editing/lines/line-with-chrome.jsx
+++ client/components/editing/lines/line-with-chrome.jsx
@@ -10,12 +10,17 @@ module.exports = React.createClass({
 			clazz = clazz + " bg-warning";
 		}
 
+        var active = null;
 		if (this.props.step.active){
-			clazz += ' active';
+			clazz += ' bg-info';
+            active = (<i className="fa fa-location-arrow"></i>);
 		}
+
+        
 
 		return (
 			<div className={clazz} id={this.props.step.id}>
+                {active}
 				<DeleteGlyph step={this.props.step} />
 				{this.props.components}
 				<ReorderGlyph step={this.props.step} />
