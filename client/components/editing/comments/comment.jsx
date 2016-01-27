fab62c643f1b8a35d028fa0b617137ee9cf062ff
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-01-27 07:21:24 -0600

    Styling comments

--- client/components/editing/comments/comment.jsx
+++ client/components/editing/comments/comment.jsx
@@ -3,12 +3,12 @@ var React = require("react");
 var Comment = React.createClass({
 	render: function(){
 		return (
-		<span 
+		<div 
 			id={this.props.step.id} 
 			className="comment" 
 			 >
 				<span className="comment-text">{this.props.arg.value}</span>
-		</span>
+		</div>
 		);
 	}
 });
