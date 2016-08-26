3a44966603bd287a74696d66bb7af21db314b6c8
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-08-26 10:50:03 -0500

    converted st doc-run to using Kestrel

--- src/ST/Docs/Runner/BrowserRefresher.cs
+++ src/ST/Docs/Runner/BrowserRefresher.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using Baseline;
 using Fleck;
 
 namespace ST.Docs.Runner
@@ -8,8 +6,6 @@ namespace ST.Docs.Runner
     public interface IBrowserRefresher
     {
         void RefreshPage();
-        void StartWebSockets();
-        int Port { get; }
     }
 
     public class NulloBrowserRefresher : IBrowserRefresher
@@ -18,66 +14,28 @@ namespace ST.Docs.Runner
         {
             // nothing
         }
-
-        public void StartWebSockets()
-        {
-            throw new NotSupportedException();
-        }
-
-        public int Port
-        {
-            get
-            {
-                throw new NotSupportedException();
-            }
-        }
     }
 
     public class BrowserRefresher : IDisposable, IBrowserRefresher
     {
-        private int _port;
-        private WebSocketServer _server;
-        private string _webSocketsAddress;
-        private readonly IList<IWebSocketConnection> _sockets = new List<IWebSocketConnection>();
+        private readonly WebSocketsHandler _handler;
 
-        public void StartWebSockets()
+        public BrowserRefresher(WebSocketsHandler handler)
         {
-            // Make sure that under no circumstances does this thing conflict w/ Raven's 8080
-            _port = PortFinder.FindPort(8200);
-            _webSocketsAddress = "ws://0.0.0.0:" + _port;
-
-            Console.WriteLine("Broadcasting auto-refresh messages via WebSockets at " + _webSocketsAddress);
-
-            _server = new WebSocketServer(_webSocketsAddress);
-            _server.Start(socket =>
-            {
-                socket.OnOpen = () => _sockets.Add(socket);
-
-                socket.OnClose = () => _sockets.Remove(socket);
-
-
-            });
+            _handler = handler;
         }
 
-        public string WebSocketsAddress
-        {
-            get { return _webSocketsAddress; }
-        }
-
-        public int Port
-        {
-            get { return _port; }
-        }
 
         public void RefreshPage()
         {
-            _sockets.Each(x => x.Send("REFRESH"));
+#pragma warning disable 4014
+            _handler.Send("REFRESH");
+#pragma warning restore 4014
         }
 
         public void Dispose()
         {
-            _sockets.Clear();
-            if (_server != null) _server.Dispose();
+            _handler.Dispose();
         }
     }
 }
