66fb96570bccce59beecc73e5e56221395452fc9
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-08-26 14:58:36 -0500

    st open is mostly on Kestrel now

--- src/ST/Client/WebApplicationRunner.cs
+++ src/ST/Client/WebApplicationRunner.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.IO;
 using Baseline;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using StoryTeller.Commands;
+using StructureMap;
 
 namespace ST.Client
 {
     public class WebApplicationRunner : IDisposable
     {
         private readonly OpenInput _input;
+        private IContainer _container;
+        private IWebHost _server;
 
         public WebApplicationRunner(OpenInput input)
         {
@@ -15,18 +23,12 @@ namespace ST.Client
 
         public RemoteController Controller { get; private set; }
 
-        public string BaseAddress
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
+        public string BaseAddress { get; private set; }
         public void Dispose()
         {
             Controller.Teardown();
-            //_server.SafeDispose();
+            _server.SafeDispose();
+            _container.Dispose();
         }
 
         public void Start()
@@ -40,32 +42,73 @@ namespace ST.Client
 
             context.Start();
 
-            throw new NotImplementedException();
-
-            /*
-            var registry = new FubuRegistry();
-
-
-            registry.AlterSettings<DiagnosticsSettings>(_ => _.TraceLevel = TraceLevel.Verbose);
-            registry.Mode = "development";
-            registry.HostWith<NOWIN>();
-            registry.Services.For<IRemoteController>().Use(Controller);
-            registry.Services.For<StorytellerContext>().Use(context);
-            
-            registry.Services.IncludeRegistry<WebApplicationRegistry>();
+            var port = PortFinder.FindPort(5000);
+            if (_input.WebSocketAddressFlag.IsNotEmpty())
+            {
+                port = new Uri(_input.WebSocketAddressFlag).Port;
+            }
 
 
-            _server = registry.ToRuntime();
-            var connector = _server.Get<IClientConnector>();
-            connector.Start();
-            Controller.AddListener(connector);
+            BaseAddress = "http://localhost:" + port;
 
-            _server.Get<AssetFileWatcher>().Start();
+            var webSockets = new WebSocketsHandler();
 
-            var persistence = _server.Get<IPersistenceController>();
+
+            var webSocketsAddress = $"ws://127.0.0.1:{port}";
+
+            // TODO -- fugly as hell. Either do it all the SM way, or rip out SM
+            var registry = new WebApplicationRegistry(webSocketsAddress, webSockets, Controller, context);
+            _container = new Container(registry);
+
+
+            var baseDirectory = AppContext.BaseDirectory;
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(baseDirectory)
+                .UseUrls($"http://localhost:{port}")
+                .Configure(app =>
+                {
+                    app.UseWebSockets();
+
+                    app.Use(async (http, next) =>
+                    {
+                        if (http.WebSockets.IsWebSocketRequest)
+                        {
+                            await webSockets.HandleSocket(http).ConfigureAwait(false);
+                        }
+                        else
+                        {
+                            await next().ConfigureAwait(false);
+                        }
+                    });
+
+                    app.UseStaticFiles(new StaticFileOptions
+                    {
+                        ServeUnknownFileTypes = true,
+                        FileProvider = new PhysicalFileProvider(baseDirectory)
+                    });
+
+                    app.Run(async (http) =>
+                    {
+                        var endpoint = _container.GetInstance<HomeEndpoint>();
+                        var html = endpoint.Index().ToString();
+
+                        http.Response.ContentType = "text/html";
+                        await http.Response.WriteAsync(html).ConfigureAwait(false);
+                    });
+                });
+
+            _server = host.Start();
+
+
+            Controller.AddListener(_container.GetInstance<IClientConnector>());
+
+            _container.GetInstance<AssetFileWatcher>().Start();
+
+            var persistence = _container.GetInstance<IPersistenceController>();
             persistence.StartWatching(context.SpecPath);
             context.AddRemoteListener(persistence);
-            */
+            
         }
     }
 }
