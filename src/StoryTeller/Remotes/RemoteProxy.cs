060f0e38611be2509e71f1c330dae8fab46e2b92
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-09-09 13:05:34 -0500

    extracted EngineAgent out of RemoteProxy

--- src/StoryTeller/Remotes/RemoteProxy.cs
+++ src/StoryTeller/Remotes/RemoteProxy.cs
@@ -1,41 +1,26 @@
 ﻿#if NET46
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Baseline;
 using StoryTeller.Engine;
-using StoryTeller.Engine.Batching;
-using StoryTeller.Engine.UserInterface;
 using StoryTeller.Messages;
-using StoryTeller.Remotes.Messaging;
 
 namespace StoryTeller.Remotes
 {
-
     public class RemoteProxy : MarshalByRefObject, IDisposable
     {
-        private object _controller;
-        private SpecificationEngine _engine;
-        private Project _project;
-        private ISystem _system;
-        private readonly IList<IDisposable> _disposables = new List<IDisposable>();
-        private SpecExpiration _specExpiration;
-        private SocketConnection _socket;
+        private readonly EngineAgent _agent;
+
+        public RemoteProxy()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomainOnDomainUnload;
+
+            _agent = new EngineAgent();
+        }
 
 
         public void Dispose()
         {
-            try
-            {
-                _disposables.ToArray().Each(x => x.SafeDispose());
-                _disposables.Clear();
-
-
-            }
-            catch (Exception e)
-            {
-                ConsoleWriter.Write(ConsoleColor.Red, e.ToString());
-            }
+            _agent.Dispose();
         }
 
         public override object InitializeLifetimeService()
@@ -45,129 +30,26 @@ namespace StoryTeller.Remotes
 
         public QueueState QueueState()
         {
-            var controller = _controller as EngineController;
-            return controller == null ? new QueueState() : controller.QueueState();
+            return _agent.QueueState();
         }
 
         public void Start(EngineMode mode, Project project, int socketPort)
         {
-            Project.CurrentProject = project;
-
-            _socket = new SocketConnection(socketPort, false, (s, json) =>
-            {
-                EventAggregator.Messaging.SendJson(json);
-            });
-            _disposables.Add(_socket);
-
-            EventAggregator.Start(_socket);
-
-            _project = project;
-
-            Type systemType = null;
-
-            try
-            {
-                systemType = _project.DetermineSystemType();
-                _system = Activator.CreateInstance(systemType).As<ISystem>();
-                _disposables.Add(_system);
-
-                _specExpiration = new SpecExpiration();
-
-                _engine = mode == EngineMode.Interactive 
-                    ? buildUserInterfaceEngine() 
-                    : buildBatchedEngine(project.TracingStyle);
-
-
-                _engine.Start(project.StopConditions);
-
-                
-            }
-            catch (Exception e)
-            {
-                var message = new SystemRecycled
-                {
-                    error = e.ToString(),
-                    success = false,
-                };
-
-                if (systemType != null)
-                {
-                    message.system_name = systemType.AssemblyQualifiedName;
-                }
-
-                EventAggregator.SendMessage(message);
-            }
-
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            AppDomain.CurrentDomain.DomainUnload += CurrentDomainOnDomainUnload;
+            _agent.Start(mode, project, socketPort);
         }
 
         private void CurrentDomainOnDomainUnload(object sender, EventArgs eventArgs)
         {
-            _engine?.SafeDispose();
-            _socket?.SafeDispose();
+            Dispose();
         }
 
-        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             Console.WriteLine("Uncaught exception, shutting down.");
             Console.WriteLine(e.ExceptionObject.ToString());
-            _engine?.Dispose();
-            _socket?.SafeDispose();
+            Dispose();
         }
-
-        private SpecificationEngine buildUserInterfaceEngine()
-        {
-            var observer = new UserInterfaceObserver();
-
-            var runner = new SpecRunner(new UserInterfaceExecutionMode(observer), _system, _specExpiration);
-
-            var executionObserver = new UserInterfaceExecutionObserver();
-            var engine  = new SpecificationEngine(_system, runner, executionObserver);
-            _controller = new EngineController(engine, observer, runner);
-
-            // Super hokey, but we need some way to feed the spec started
-            // event up to EngineController
-            // TODO -- maybe pull the IExecutionQueue concept from 1-2 back out
-            executionObserver.Controller = (EngineController) _controller;
-
-
-            _disposables.Add(observer);
-            _disposables.Add(engine);
-
-            EventAggregator.Messaging.AddListener(_controller);
-
-            return engine;
-        }
-
-        private SpecificationEngine buildBatchedEngine(string tracingStyle)
-        {
-            IBatchObserver batchObserver = new BatchObserver();
-            IExecutionObserver executionObserver = new NulloObserver();
-
-            if ("TeamCity" == tracingStyle)
-            {
-                batchObserver = new TeamCityBatchObserver(batchObserver);
-                executionObserver = new TeamCityExecutionObserver();
-            }
-
-            var executionMode = new BatchExecutionMode(batchObserver);
-            var runner = new SpecRunner(executionMode, _system, _specExpiration);
-
-            var engine = new SpecificationEngine(
-                _system, runner, executionObserver);
-
-            _controller = new BatchController(engine, batchObserver);
-
-            _disposables.Add(engine);
-
-            EventAggregator.Messaging.AddListener(_controller);
-
-            return engine;
-        }
-
     }
-
 }
 
 #endif
