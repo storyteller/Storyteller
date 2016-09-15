fa726cac1a2a2bb72514e8b47eb14a7dc58eae80
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-09-15 08:03:16 -0500

    cleanly shutting down the spec editor in coreclr. Closes GH-527

--- src/StoryTeller/Remotes/SystemRecycled.cs
+++ src/StoryTeller/Remotes/SystemRecycled.cs
@@ -43,7 +43,13 @@ namespace StoryTeller.Remotes
         public void WriteSystemUsage()
         {
             Console.WriteLine("Using System: " + system_name);
-            properties.Each(pair => { Console.WriteLine("{0}: {1}", pair.Key, pair.Value); });
+            properties.Each(pair =>
+            {
+                if (pair.Value != null)
+                {
+                    Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+                }
+            });
             Console.WriteLine();
         }
     }
