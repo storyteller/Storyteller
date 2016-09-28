89d5b704f2cf9fa6c362f46e3a80f5af7fc25644
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-09-28 08:05:42 -0500

    just proving out the critical exception stops a test

---
+++ src/Samples/Fixtures/CriticalThrowingFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StoryTeller;
+
+namespace Samples.Fixtures
+{
+    public class CriticalThrowingFixture : Fixture
+    {
+        public void ThrowNormal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowCritical()
+        {
+            throw new StorytellerCriticalException("No!");
+        }
+
+        public IGrammar SetupBlows()
+        {
+            return Embed<SetupBlowupFixture>("Blows up in Setup");
+        }
+
+    }
+
+    public class SetupBlowupFixture : Fixture
+    {
+        public override void SetUp()
+        {
+            throw new StorytellerCriticalException("Stop!");
+        }
+
+        public void Go()
+        {
+            
+        }
+    }
+}
