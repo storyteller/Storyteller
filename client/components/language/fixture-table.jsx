fee6111d74b646a635cda66b856ea97f206f327c
jeremydmiller <jeremydmiller@yahoo.com>
2016-11-10 08:30:00 -0600

    showing a preview and editor for a fixture in the fixture explorer

--- client/components/language/fixture-table.jsx
+++ client/components/language/fixture-table.jsx
@@ -2,55 +2,29 @@ import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 const Specification = require('../../lib/model/specification');
-import { preview as loader } from './../editing/component-loader';
-import { Grid, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { preview, editing } from './../editing/component-loader';
+import { Grid, Row, Col, Tabs, Tab } from 'react-bootstrap';
 
-function GrammarTable({fixture, spec}){
-    const grammars = _.sortBy(_.values(fixture.grammars), x => x.title);
-
-    loader.reset();
-    const components = spec.previews(loader);
-
-    return (
-      <Grid>
-        <Row>
-          {components}
-        </Row>
-      </Grid>
-    );
-
-/*
-    const rows = grammars.map((x, i) => {
-      return (
-        <tr key={i+1}>
-          <td>{x.title}</td>
-          <td>{x.type}</td>
-        </tr>
-      );
-    });
-
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    );
-    */
-}
 
 function FixtureTable({fixture, spec}){
+  const previews = spec.previews(preview);
+  const editors = spec.editors(editing);
+
+
+
   return (
     <div>
       <h2>Fixture '{fixture.title}' ({fixture.implementation})</h2>
       <hr />
-      <GrammarTable fixture={fixture} spec={spec} />
+      <Grid>
+        <Row>
+          <p>Below is a sample specification for this fixture just to preview its usage</p>
+          <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+            <Tab eventKey={1} title="Preview">{previews}</Tab>
+            <Tab eventKey={2} title="Editing">{editors}</Tab>
+          </Tabs>
+        </Row>
+      </Grid>
     </div>
   );
 }
