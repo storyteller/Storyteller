026e2671eb931e1071eb6602510c6a5db6ea2dee
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-12-13 08:11:00 -0600

    Added the new dump-fixtures command. Closes GH-619

---
+++ src/dotnet-storyteller/CommandLine/DumpFixturesCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using Baseline;
+using Oakton;
+using StoryTeller.Model;
+using StoryTeller.Model.Persistence.DSL;
+using StoryTeller.Remotes;
+using ST.Client;
+
+namespace ST.CommandLine
+{
+    public class DumpFixturesInput : ProjectInput
+    {
+        public DumpFixturesInput() : base(EngineMode.ExportOnly)
+        {
+        }
+    }
+
+    [Oakton.Description("Exports all of the Fixture definitions to markdown for review or using within the headless mode", Name = "dump-fixtures")]
+    public class DumpFixturesCommand : OaktonCommand<DumpFixturesInput>
+    {
+        public override bool Execute(DumpFixturesInput input)
+        {
+            if (!Directory.Exists(input.FixturePath))
+            {
+                Console.WriteLine("Creating directory " + input.FixturePath);
+                Directory.CreateDirectory(input.FixturePath);
+            }
+
+            using (var controller = input.BuildEngine())
+            {
+                var loading = controller.Start();
+
+                var reading = Task.Factory.StartNew(() => FixtureLoader.LoadFromPath(input.FixturePath));
+
+                Task.WaitAll(loading, reading);
+
+                var combined = FixtureLibrary.From(loading.Result.fixtures)
+                    .ApplyOverrides(reading.Result);
+
+                foreach (var fixture in combined.Models)
+                {
+                    var file = input.FixturePath.AppendPath(fixture.key + ".md");
+                    try
+                    {
+                        FixtureWriter.Write(fixture, file);
+                    }
+                    catch (Exception e)
+                    {
+                        ConsoleWriter.Write(ConsoleColor.Red, $"Could not export fixture {fixture.key}");
+                        ConsoleWriter.Write(ConsoleColor.Yellow, e.ToString());
+                    }
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
