6f069c7152821e5b84416bd3ab68edad25ee640d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-01-26 14:04:58 -0600

    Missed in last commit

--- src/Storyteller.AspNetCore/AspNetCoreSystem.cs
+++ src/Storyteller.AspNetCore/AspNetCoreSystem.cs
@@ -13,23 +13,28 @@ namespace Storyteller.AspNetCore
     {
         public static void Run<T>(string[] args) where T : class
         {
-            var system = ForStartup<T>();
+            var system = new AspNetCoreSystem(() => SystemUnderTest.ForStartup<T>());
             StorytellerAgent.Run(args, system);
         }
 
-        public static AspNetCoreSystem ForStartup<T>() where T : class
-        {
-            var system = new AspNetCoreSystem();
-            system.Startup<T>();
 
-            return system;
-        }
 
         public readonly CellHandling CellHandling = new CellHandling(new EquivalenceChecker(), new Conversions());
 
-        private Func<ISystemUnderTest> _systemBuilder;
         private ISystemUnderTest _systemUnderTest;
-        private Task<ISystemUnderTest> _warmup;
+        private readonly Task<ISystemUnderTest> _warmup;
+
+        private AspNetCoreSystem(Func<ISystemUnderTest> builder)
+        {
+            _warmup = Task.Factory.StartNew(() =>
+            {
+                var sut = builder();
+
+                beforeAll(sut);
+
+                return sut;
+            });
+        }
 
         // TODO -- add one that uses Action<IApplicationBuilder> for more adhoc things
 
@@ -40,19 +45,6 @@ namespace Storyteller.AspNetCore
 
         public CellHandling Start()
         {
-            if (_systemBuilder == null)
-                throw new InvalidOperationException(
-                    $"You must call {nameof(Startup)} to declare a Startup type for your application");
-
-            _warmup = Task.Factory.StartNew(() =>
-            {
-                var sut = _systemBuilder();
-
-                beforeAll(sut);
-
-                return sut;
-            });
-
             return CellHandling;
         }
 
@@ -71,11 +63,6 @@ namespace Storyteller.AspNetCore
         public Task Warmup()
         {
             return _warmup;
-        }
-
-        protected void Startup<T>() where T : class
-        {
-            _systemBuilder = () => SystemUnderTest.ForStartup<T>().As<ISystemUnderTest>();
         }
 
         protected virtual void beforeAll(ISystemUnderTest sut)
