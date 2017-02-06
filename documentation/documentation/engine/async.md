2efae4ad9e48b955cdcf15ba37a948c0a11134f0
jeremydmiller <jeremydmiller@yahoo.com>
2017-02-06 12:46:58 -0600

    added some docs on using asynchronous methods as fixture grammars

---
+++ documentation/documentation/engine/async.md
@@ -0,0 +1,10 @@
+<!--title: Asynchronous Grammars-->
+
+Storyteller 4.0 adds _some_ ability to use asynchronous methods as grammars. As asynchronous programming
+continues to become so much more prevalant, Storyteller will adapt and add more capabilities. For now,
+you have these options:
+
+<[sample:AsyncOperationsFixture]>
+
+Effectively, all you're doing is allowing Storyteller to worry about the `Task`'s much the same way testing
+tools like xUnit or mocha do for you today.
---
+++ src/Samples/Fixtures/AsyncOperationsFixture.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using StoryTeller;
+
+namespace Samples.Fixtures
+{
+    // SAMPLE: AsyncOperationsFixture
+    public class AsyncOperationsFixture : Fixture
+    {
+        [FormatAs("Perform a task asynchronously")]
+        public Task PerformTask()
+        {
+            // do something with a method that returns a Task
+            return Task.CompletedTask;
+        }
+
+        [FormatAs("The current name should be {name}")]
+        public Task<string> CheckName()
+        {
+            // Use an ansynchronous method
+            return Task.FromResult("Jeremy");
+        }
+
+        [FormatAs("This condition should be true")]
+        public Task<bool> IsConditionTrue()
+        {
+            // Use an asynchronous result
+            return Task.FromResult(true);
+        }
+
+    }
+    // ENDSAMPLE
+}
