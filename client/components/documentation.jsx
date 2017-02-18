88f914a0e73e344e69332e5cce6be4a61be76eef
Victor Grigoriu <vgrigoriu@gmail.com>
2017-02-18 18:44:36 +0200

    Update version number in documentation component

--- client/components/documentation.jsx
+++ client/components/documentation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Documentation() {
   return (
     <div>
-      <h4>The documentation for Storyteller 3 is available at <a href="http://storyteller.github.io">http://storyteller.github.io</a></h4>
+      <h4>The documentation for Storyteller 4 is available at <a href="http://storyteller.github.io">http://storyteller.github.io</a></h4>
     </div>
   );
 }
