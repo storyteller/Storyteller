67ff2f0fc28b75c22d0128167bd292c44e7de031
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-02-21 09:44:57 -0600

    spiking in quite a bit of Storyteller.Selenium

---
+++ src/Storyteller.Selenium.Samples/SampleFixture.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using StoryTeller;
+
+namespace Storyteller.Selenium.Samples
+{
+    public class SampleFixture : ScreenFixture
+    {
+        public SampleFixture()
+        {
+            Element("the Show button", By.Id("button1"));
+            Element("the Hide button", By.Id("button2"));
+            Element("the div", By.Id("div1"));
+            Element("the textbox", By.Id("text1"));
+        }
+
+        protected override void beforeRunning()
+        {
+            var path = "file://" + Project.CurrentProject.ProjectPath.Replace("\\", "/");
+            Context.State.Store(RootUrl, path);
+        }
+
+
+    }
+}
