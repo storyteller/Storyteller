6db70332413027ef2da71cdd2c83682d45e6e15e
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-02-21 19:28:59 -0600

    added back StorytellerRunner for easier debugging. Closes GH-655

---
+++ src/Specifications/Debugger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Baseline;
+using StoryTeller;
+
+namespace Specifications
+{
+    public class Debugger
+    {
+        public void DoStuff()
+        {
+            using (var runner = StorytellerRunner.For<SpecificationSystem>())
+            {
+                //runner.RunAll(5.Minutes());
+
+                runner.Run("Docs / Advanced LinkTo Transformations");
+
+                runner.OpenResultsInBrowser();
+            }
+        }
+    }
+}
