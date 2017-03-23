bcda2dfad7ce6f5c846ad62058b04284b9e42f28
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-03-23 12:10:30 -0500

    can open url's with windows + netcoreapp1.0

--- src/StoryTeller/ProcessLauncher.cs
+++ src/StoryTeller/ProcessLauncher.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
-using Platform = Microsoft.DotNet.InternalAbstractions.Platform;
-using RuntimeEnvironment = Microsoft.DotNet.InternalAbstractions.RuntimeEnvironment;
+using System.IO;
+using Microsoft.DotNet.InternalAbstractions;
 
 namespace StoryTeller
 {
@@ -16,12 +16,12 @@ namespace StoryTeller
 #if NET46
                     Process.Start(url);
 #else
-                    Process.Start("cmd", $"/C start /D {url}");
+                    Process.Start("cmd", $"/C start {url}");
 #endif
                 }
                 else
                 {
-                    Console.WriteLine("Trying to open file " + url);
+                    Console.WriteLine("Trying to open url " + url);
 
                     Process.Start(new ProcessStartInfo
                     {
@@ -35,17 +35,58 @@ namespace StoryTeller
             {
                 ConsoleWriter.Write(ConsoleColor.Yellow, "Unable to launch the browser to " + url);
             }
-
         }
 
         public static void OpenFile(string file)
         {
-            if (file.Contains(" "))
-            {
-                file = "\"" + file + "\"";
-            }
 
-            GotoUrl(file);
+
+            try
+            {
+                if (RuntimeEnvironment.OperatingSystemPlatform == Platform.Windows)
+                {
+#if NET46
+                    if (file.Contains(" "))
+                        file = "\"" + file + "\"";
+                    Process.Start(file);
+#else
+
+
+                    var workingDirectory = Baseline.StringExtensions.ParentDirectory(file);
+//                    if (workingDirectory.Contains(" "))
+//                    {
+//                        workingDirectory = "\"" + workingDirectory + "\"";
+//                    }
+
+                    Process.Start(new ProcessStartInfo
+                    {
+                        UseShellExecute = false,
+                        FileName = "cmd",
+                        WorkingDirectory = workingDirectory,
+                        Arguments = $"start /D {Path.GetFileName(file)}"
+                    });
+#endif
+                }
+                else
+                {
+                    Console.WriteLine("Trying to open file " + file);
+
+                    if (file.Contains(" "))
+                        file = "\"" + file + "\"";
+
+                    Process.Start(new ProcessStartInfo
+                    {
+                        
+                        UseShellExecute = false,
+                        FileName = "open",
+                        Arguments = file
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                ConsoleWriter.Write(ConsoleColor.Yellow, "Unable to launch the browser to " + file);
+            }
         }
     }
 }
