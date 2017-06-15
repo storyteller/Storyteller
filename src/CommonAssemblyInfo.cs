d4f04a1b71fca3ec58e98057bfb0fb184048003b
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-06-15 15:16:15 -0500

    Fixing more version problems

--- src/CommonAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyDescription("IoC Container for .Net")]
-[assembly: AssemblyProduct("Executable Specifications for .Net")]
-[assembly: AssemblyCopyright("Copyright 2008-2017 Jeremy D. Miller, Brandon Behrens, Andrew Kharlamov, et al. All rights reserved.")]
-[assembly: AssemblyTrademark("f27ae3b9cdd10781529e7dc4deb3b936dbe56aba")]
-[assembly: AssemblyVersion("4.2.0.51346")]
-[assembly: AssemblyFileVersion("4.2.0.51346")]
-[assembly: AssemblyInformationalVersion("4.2.0.51346")]
