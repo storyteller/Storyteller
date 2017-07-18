e125a33dbcefd0fe62f64915f6eb210edbe7060d
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-07-17 21:59:44 -0400

    Add code comments to IRunExecutionContext

--- src/StoryTeller/Engine/IRunExecutionContext.cs
+++ src/StoryTeller/Engine/IRunExecutionContext.cs
@@ -4,7 +4,16 @@ namespace StoryTeller.Engine
 {
     public interface IRunExecutionContext
     {
+        /// <summary>
+        /// Executes right before the provided specifications are run
+        /// </summary>
+        /// <param name="specifications">All specifications which are about to be executed</param>
         void BeforeRun(Specification[] specifications);
+
+        /// <summary>
+        /// Executes right after all speicifications have been executed and provides the results for all of them.
+        /// </summary>
+        /// <param name="results">Results of the executed specifications</param>
         void AfterRun(BatchRecord[] results);
     }
 }
