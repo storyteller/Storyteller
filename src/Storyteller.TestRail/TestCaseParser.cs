c429a498713167ed7b24903834e61385805190de
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-08-17 14:18:40 -0400

    Update TestCaseParser to look for brackets [] around C# to avoid inadvertantly picking up C#s in other parts of spec name

--- src/Storyteller.TestRail/TestCaseParser.cs
+++ src/Storyteller.TestRail/TestCaseParser.cs
@@ -6,7 +6,7 @@ namespace StoryTeller.TestRail
 {
     public class TestCaseParser
     {
-        private static readonly Regex _testCaseRegex = new Regex("([Cc][0-9]+)", RegexOptions.Compiled);
+        private static readonly Regex _testCaseRegex = new Regex(@"([\[][Cc][0-9]+[\]])", RegexOptions.Compiled);
 
         public static IEnumerable<int> ParseTestCaseIds(string str)
         {
@@ -19,7 +19,7 @@ namespace StoryTeller.TestRail
             {
                 if (match.Success)
                 {
-                    yield return Convert.ToInt32(match.Value.ToUpper().Replace("C", string.Empty));
+                    yield return Convert.ToInt32(match.Value.ToUpper().Replace("[C", string.Empty).Replace("]", string.Empty));
                 }
             }
         }
