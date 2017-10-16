bad545a148c8a6144b0b061738532d6baeb5135a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-10-16 10:49:52 -0500

    Can look up target framework in OpenInput now

---
+++ src/StoryTeller.Testing/ST/OpenInputTests.cs
@@ -0,0 +1,46 @@
+﻿using Shouldly;
+using ST.Client;
+using Xunit;
+
+namespace StoryTeller.Testing.ST
+{
+    public class OpenInputTests
+    {
+        [Fact]
+        public void can_determine_framework_from_project_file_chooses_only()
+        {
+            var input = new OpenInput
+            {
+                PathFlag = TestingContext.FindParallelDirectory("DatabaseSamples")
+
+            };
+            
+            input.DetermineFramework().ShouldBe("netcoreapp1.1");
+        }
+        
+        [Fact]
+        public void can_determine_framework_from_project_file_choose_first()
+        {
+            var input = new OpenInput
+            {
+                PathFlag = TestingContext.FindParallelDirectory("Samples")
+
+            };
+            
+            input.DetermineFramework().ShouldBe("net46");
+        }
+
+        [Fact]
+        public void override_framework_with_flag()
+        {
+            var input = new OpenInput
+            {
+                FrameworkFlag = "netcoreapp1.1",
+                PathFlag = TestingContext.FindParallelDirectory("Samples")
+
+            };
+            
+            input.DetermineFramework().ShouldBe("netcoreapp1.1");
+        }
+    }
+}
--- src/dotnet-storyteller/Client/OpenInput.cs
+++ src/dotnet-storyteller/Client/OpenInput.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Baseline;
 using Oakton;
 using StoryTeller;
@@ -12,6 +14,32 @@ namespace ST.Client
 {
     public class OpenInput : StorytellerInput
     {
+        public string DetermineFramework()
+        {
+            if (FrameworkFlag.IsNotEmpty()) return FrameworkFlag;
+
+            var projectFile = new FileSystem().FindFiles(PathFlag, FileSet.Shallow("*.csproj")).FirstOrDefault();
+            if (projectFile == null) throw new InvalidOperationException($"Cannot find a csproj file in directory {PathFlag}. Try explicitly setting the --framework flag value");
+
+            var document = new XmlDocument();
+            using (var file = new FileStream(projectFile, FileMode.Open))
+            {
+                document.Load(file);
+            }
+
+            var elements = document.DocumentElement.GetElementsByTagName("TargetFrameworks");
+            if (elements.Count == 0)
+            {
+                elements = document.DocumentElement.GetElementsByTagName("TargetFramework");
+            }
+            
+            if (elements.Count == 0) throw new InvalidOperationException("Unable to determine the project target framework. Try explicitly setting the --framework flag");
+
+            var element = elements[0];
+
+            return element.InnerText.ToDelimitedArray(';').First();
+        }
+        
         [Description("Override the .Net target framework if you are multi-targeting")]
         public string FrameworkFlag { get; set; }    
         
