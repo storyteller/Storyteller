e77bbc55f4027f334cb8ddd05593003b5ab21a42
jeremydmiller <jeremydmiller@yahoo.com>
2018-07-23 09:19:02 -0500

    very, very basic dotnet new template for storyteller specs

---
+++ templates/Storyteller.Specifications/content/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using StoryTeller;
+
+namespace StorytellerSpecs
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            return StorytellerAgent.Run(args);
+        }
+    }
+}
