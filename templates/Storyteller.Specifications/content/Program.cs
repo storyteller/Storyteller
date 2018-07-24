899a5ce0965366b371e70b1f8f1b4ae75ac161cd
jeremydmiller <jeremydmiller@yahoo.com>
2018-07-24 07:51:07 -0500

    basic dotnet template seems to be done

--- templates/Storyteller.Specifications/content/Program.cs
+++ templates/Storyteller.Specifications/content/Program.cs
@@ -1,13 +1,48 @@
 using System;
 using StoryTeller;
+using StoryTeller.Engine;
+using System.Threading.Tasks;
 
 namespace StorytellerSpecs
 {
-    internal class Program
+    internal class Program : StoryTeller.Engine.SimpleSystem
     {
         public static int Main(string[] args)
         {
-            return StorytellerAgent.Run(args);
+            return StorytellerAgent.Run<Program>(args);
+        }
+
+        protected override void configureCellHandling(CellHandling handling)
+        {
+            // Allows you to apply customizations to CellHandling, register
+            // custom equality checks, custom type converters, register
+            // system level data lists
+        }
+
+        public override void BeforeEach(SimpleExecutionContext execution, ISpecContext context)
+        {
+            // Executes before each specification run
+            // Handy for setting up expected state or cleaning
+            // up state left over from previous tests
+        }
+
+        public override void AfterEach(ISpecContext context)
+        {
+            // Executes after each specification run
+            // Handy for state cleanup
+        }
+
+        public override Task Warmup()
+        {
+            // do any necessary bootstrapping work to get
+            // your system ready to use before the first specification
+            // runs
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            // any kind of system cleanup when Storyteller is shutting down
         }
     }
 }
