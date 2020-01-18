63c8ff6db236783211d4252bf72342c837340a59
Sergei Golos <sergei.golos@gmail.com>
2020-01-18 17:30:07 -0500

    Create DefaultHtmlDocumentBuilder.cs

---
+++ src/StoryTeller/Results/DefaultHtmlDocumentBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace StoryTeller.Results
+{
+    /// <summary>
+    /// Defines the document builders required to bootstrap the default script and styles
+    /// for storyteller. 
+    /// </summary>
+    public class DefaultHtmlDocumentBuilder : HtmlDocumentBuilder
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="DefaultHtmlDocumentBuilder"/> by registering the
+        /// required JS and CSS bundles along with the packaged report results.  
+        /// </summary>
+        public DefaultHtmlDocumentBuilder()
+        {
+            var jsBundle = new StoryTellerResourceLoader("StoryTeller.batch-bundle.js");
+            var cssBundle = new CombinedResourceLoader()
+                .AddLoader(new StoryTellerResourceLoader("Storyteller.stylesheets.bootstrap.min.css"))
+                .AddLoader(new StoryTellerResourceLoader("StoryTeller.stylesheets.storyteller.css"))
+                .AddLoader(new StoryTellerResourceLoader("StoryTeller.stylesheets.fixed-data-table.min.css"));
+
+            this.Add(new StoryTellerTitleBuilder());
+            this.Add(new StyleTagBuilder(cssBundle));
+            this.Add(new ReportPartBuilder());
+            this.Add(new ScriptTagBuilder(jsBundle));
+        }
+    }
+}
