e249a565bae1873b5df7106dec9d708cd635ef54
Sergei Golos <sergei.golos@gmail.com>
2020-01-18 20:12:35 -0500

    added missing files

---
+++ src/StoryTeller.Testing/Results/DefaultHtmlDocumentBuilderTester.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Shouldly;
+using StoryTeller.Results;
+using Xunit;
+
+namespace StoryTeller.Testing.Results
+{
+    public class DefaultHtmlDocumentBuilderTester
+    {
+
+        [Fact]
+        public void additional_builders_can_be_added()
+        {
+            var builder = new DefaultHtmlDocumentBuilder();
+                builder.Add(new ScriptTagBuilder(string.Empty));
+                
+            builder.Get<IDocumentBuilder>().Count().ShouldBe(5);
+        }
+        
+        [Fact]
+        public void defines_four_parts()
+        {
+            var builder = new DefaultHtmlDocumentBuilder();
+            builder.Get<IDocumentBuilder>().Count().ShouldBe(4);
+        }
+        
+        [Fact]
+        public void defines_the_title()
+        {
+            var builder = new DefaultHtmlDocumentBuilder();
+            builder.Get<StoryTellerTitleBuilder>().ShouldNotBeEmpty();
+        }
+        
+        [Fact]
+        public void defines_the_javascript()
+        {
+            var builder = new DefaultHtmlDocumentBuilder();
+            builder.Get<ScriptTagBuilder>().ShouldNotBeEmpty();
+        }
+        
+        
+        [Fact]
+        public void defines_the_styles()
+        {
+            var builder = new DefaultHtmlDocumentBuilder();
+            builder.Get<StyleTagBuilder>().ShouldNotBeEmpty();
+        }
+        
+        
+        [Fact]
+        public void defines_the_report_content()
+        {
+            var builder = new DefaultHtmlDocumentBuilder();
+            builder.Get<ReportPartBuilder>().ShouldNotBeEmpty();
+        }
+        
+    }
+}
---
+++ src/StoryTeller/Results/CompoundResourceLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Baseline;
+
+namespace StoryTeller.Results
+{
+    /// <summary>
+    /// Joins multiple <see cref="IDocumentPartLoader"/> contents into a single resource.
+    /// </summary>
+    public class CompoundResourceLoader : IDocumentPartLoader
+    {
+        private readonly string delimiter;
+        private List<IDocumentPartLoader> sources;
+
+        /// <summary>
+        /// Creates an instance of <see cref="CompoundResourceLoader"/>.
+        /// </summary>
+        /// <param name="delimiter">The join delimiter. Default is two new lines.</param>
+        public CompoundResourceLoader(string delimiter = "\n\n")
+        {
+            this.delimiter = delimiter;
+            this.sources = new List<IDocumentPartLoader>();
+        }
+
+        /// <summary>
+        /// Add a loader to the joined content.
+        /// </summary>
+        /// <param name="loader"></param>
+        /// <returns></returns>
+        public CompoundResourceLoader AddLoader(IDocumentPartLoader loader)
+        {
+            this.sources.Add(loader);
+            return this;
+        }
+        
+        /// <summary>
+        /// Renders the content of each child into one string.
+        /// </summary>
+        /// <returns>The text of all child loaders.</returns>
+        public string Read()
+        {
+            return string.Join(this.delimiter, sources.Select(n => n.Read()));
+        }
+    }
+}
