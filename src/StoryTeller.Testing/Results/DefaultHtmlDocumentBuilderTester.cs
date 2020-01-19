226fd22768587557bdca54a3553c72814f3f2f12
Sergei Golos <sergei.golos@gmail.com>
2020-01-18 23:06:09 -0500

    Added some tests to validate the HtmlDocumentBuilder.Build method.

--- src/StoryTeller.Testing/Results/DefaultHtmlDocumentBuilderTester.cs
+++ src/StoryTeller.Testing/Results/DefaultHtmlDocumentBuilderTester.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using Shouldly;
+using StoryTeller.Engine;
 using StoryTeller.Results;
+using StoryTeller.Util;
 using Xunit;
 
 namespace StoryTeller.Testing.Results
@@ -53,6 +55,61 @@ namespace StoryTeller.Testing.Results
             var builder = new DefaultHtmlDocumentBuilder();
             builder.Get<ReportPartBuilder>().ShouldNotBeEmpty();
         }
+
+        [Fact]
+        public void building_the_document_results_in_html_document()
+        {
+            var response = new BatchRunResponse();
+            var builder = new DefaultHtmlDocumentBuilder();
+            builder.Build(response).ShouldBeOfType<HtmlDocument>();
+        }
         
+        [Fact]
+        public void result_has_set_title()
+        {
+            var response = new BatchRunResponse() { system = "system", suite = "suite"};
+            var builder = new DefaultHtmlDocumentBuilder();
+            var result = builder.Build(response);
+            
+            result.Head.Children[0].TagName().ShouldBe("title");
+            result.Head.Children[0].Text().ShouldBe("Storyteller Batch Results for system: suite");
+        }
+        
+        [Fact]
+        public void result_has_set_the_style()
+        {
+            var response = new BatchRunResponse() { system = "system", suite = "suite"};
+            var builder = new DefaultHtmlDocumentBuilder();
+            var result = builder.Build(response);
+            
+            result.Head.Children[1].TagName().ShouldBe("style");
+            result.Head.Children[1].Text().ShouldNotBeEmpty();
+        }
+        
+        [Fact]
+        public void result_has_set_the_report()
+        {
+            var response = new BatchRunResponse() { system = "system", suite = "suite"};
+            var builder = new DefaultHtmlDocumentBuilder();
+            var result = builder.Build(response);
+            
+            result.Body.Children[0].TagName().ShouldBe("div");
+            result.Body.Children[0].Text().ShouldNotBeEmpty();
+            
+            result.Body.Children[1].TagName().ShouldBe("div");
+            result.Body.Children[1].Text().ShouldBeEmpty();
+        }
+        
+        [Fact]
+        public void result_has_set_the_script()
+        {
+            var response = new BatchRunResponse() { system = "system", suite = "suite"};
+            var builder = new DefaultHtmlDocumentBuilder();
+            var result = builder.Build(response);
+            
+            result.Body.Children[2].TagName().ShouldBe("script");
+            result.Body.Children[2].Text().ShouldNotBeEmpty();
+        }
+
     }
 }
